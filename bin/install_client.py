21b776dc987609d764bd218a0cdb2fe0328ede23
cliffckerr <questionably@gmail.com>
2019-01-15 19:35:51 -0500

    clean install

--- bin/install_client.py
+++ bin/install_client.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # Install any missing client modules.
-# Version: 2019jan10
+# Version: 2019jan15
 
 import os
 parentfolder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.chdir(parentfolder)
+os.system('rm -r node_modules')
 os.system('npm install')
