bed6784822583f99b53dd65b525eff6212d49bfe
cliffckerr <questionably@gmail.com>
2019-01-15 19:42:54 -0500

    test commit

--- atomica_apps/version.py
+++ atomica_apps/version.py
@@ -6,7 +6,7 @@ Standard location for module version number and date.
 
 import sciris as sc
 
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 __versiondate__ = "2019-01-15"
 __gitinfo__ = sc.gitinfo(__file__, verbose=False)
 
