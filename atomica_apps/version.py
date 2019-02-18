460e779c088fd0f1f6347ec935ac73dac5ef02c7
cliffckerr <questionably@gmail.com>
2019-02-19 00:49:50 +1100

    increment version

--- atomica_apps/version.py
+++ atomica_apps/version.py
@@ -6,7 +6,7 @@ Standard location for module version number and date.
 
 import sciris as sc
 
-__version__ = "1.1.3"
-__versiondate__ = "2019-01-15"
+__version__ = "1.2.0"
+__versiondate__ = "2019-02-08"
 __gitinfo__ = sc.gitinfo(__file__, verbose=False)
 
