8f2c22b4d793179b0b7de3b4d14fb9cb14847259
George Chadderdon <gchadder3@gmail.com>
2019-04-10 16:16:25 +1000

    Improved `install_client.py` script to work better on Windows

--- bin/install_client.py
+++ bin/install_client.py
@@ -2,8 +2,13 @@
 # Install any missing client modules.
 # Version: 2019jan15
 
-import os
+import os, shutil
 parentfolder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 os.chdir(parentfolder)
-os.system('rm -rf package-lock.json ./dist ./node_modules/')
+if os.path.exists('package-lock.json'):
+    os.remove('package-lock.json')
+if os.path.exists('./dist'):    
+    shutil.rmtree('./dist')
+if os.path.exists('./node_modules/'):
+    shutil.rmtree('./node_modules/')
 os.system('npm install')
