36318ee8dbc4a680c0243606c0ff23475dbeae30
cliffckerr <questionably@gmail.com>
2019-04-12 10:52:53 +1000

    calling 2.0

--- atomica_apps/version.py
+++ atomica_apps/version.py
@@ -6,7 +6,7 @@ Standard location for module version number and date.
 
 import sciris as sc
 
-__version__ = "1.3.0"
-__versiondate__ = "2019-03-13"
+__version__ = "2.0.0"
+__versiondate__ = "2019-04-12"
 __gitinfo__ = sc.gitinfo(__file__, verbose=False)
 
