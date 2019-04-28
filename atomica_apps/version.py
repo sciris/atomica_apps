aff639684eaf538caacffa7c42e601024cdf393a
cliffckerr <questionably@gmail.com>
2019-04-28 19:19:29 +0100

    bump version

--- atomica_apps/version.py
+++ atomica_apps/version.py
@@ -6,7 +6,7 @@ Standard location for module version number and date.
 
 import sciris as sc
 
-__version__ = "2.0.0"
-__versiondate__ = "2019-04-12"
+__version__ = "2.0.1"
+__versiondate__ = "2019-04-28"
 __gitinfo__ = sc.gitinfo(__file__, verbose=False)
 
